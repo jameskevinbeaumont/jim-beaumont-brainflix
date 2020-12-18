b7841577cfddf2a6b294ec364547dbc131c8ba95
Jim Beaumont <jameskevinbeaumont@gmail.com>
2020-12-17 21:48:16 -0500

    Have the video list updating correctly when video selected - sprint-2

--- sprint-2/src/components/Home/Home.jsx
+++ sprint-2/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './Home.scss';
-import { InitializeMVO } from '../../js/loadSampleData';
+import { InitializeAVD } from '../../js/loadSampleData';
 import MainVideo from '../MainVideo/MainVideo';
 import VideoDesc from '../VideoDesc/VideoDesc';
 import NewComment from '../NewComment/NewComment';
@@ -13,21 +13,25 @@ class Home extends React.Component {
 
     state = {
         videos: [],
-        mainvideo: InitializeMVO()
+        activeVideo: [{ id: '', title: '', channel: '', image: '' }],
+        activeVideoDetail: InitializeAVD()
     };
 
     componentDidMount() {
         this._isMounted = true;
-        console.log('componentDidMount - Home.jsx');
+        // console.log('componentDidMount - Home.jsx');
 
         axios.get(window.$BF_URL + window.$BF_VIDEOS + window.$BF_API_KEY)
             .then(result => {
                 let firstVideo = result.data[0]
+                // console.log('firstVideo: ', firstVideo)
                 let sideVideos = result.data.splice(1, result.data.length - 1)
+                // console.log('sideVideoes: ', sideVideos)
                 this.setState({ videos: sideVideos })
                 axios.get(window.$BF_URL + window.$BF_VIDEOS + firstVideo.id + window.$BF_API_KEY)
                     .then(result => {
-                        this.setState({ mainvideo: result.data })
+                        this.setState({ activeVideoDetail: result.data })
+                        this.setState({ activeVideo: firstVideo })
                     })
                     .catch(err => console.log('Error=>', err.response))
             })
@@ -35,24 +39,48 @@ class Home extends React.Component {
     };
 
     componentDidUpdate() {
-        console.log('componentDidUpdate - Home.jsx');
+        // console.log('componentDidUpdate - Home.jsx');
 
         const rprops = this.props;
 
-        console.log(rprops);
-        console.log(this.state.videos);
-        if (Object.keys(rprops.match.params).length !== 0) {
-            console.log(console.log('match.params: ', rprops.match.params))
-        };
+        // console.log(rprops);
+        // console.log(this.state.videos);
+        // console.log('activeVideo: ', this.state.activeVideo);
+        // console.log('activeVideoDetail: ', this.state.activeVideoDetail);
+        // if (Object.keys(rprops.match.params).length !== 0) {
+        //     console.log(console.log('match.params: ', rprops.match.params))
+        // };
 
-        const video = this.state.videos.find(({ id }) => id === rprops.match.params.id);
+        const video = this.state.videos.find(({ id }) =>
+            id === rprops.match.params.id);
 
-        if (video) {
-            console.log(video);
-            console.log(video.id);
+        if (video && this.state.activeVideoDetail.id !== rprops.match.params.id) {
+            // console.log('video: ', video);
+            // console.log('video.id: ', video.id);
+            // let newActiveVideoObj = { id: '', title: '', channel: '', image: '' };
+            let arrIndex = this.state.videos.findIndex(video =>
+                video.id === rprops.match.params.id);
+            // console.log('arrIndex: ', arrIndex);
+            let newActiveVideo = this.state.videos[arrIndex];
+
+            //this.state.videos.filter(video => video.id === rprops.match.params.id);
+            // newActiveVideoObj.id = newActiveVideo[0].id;
+            // newActiveVideoObj.title = newActiveVideo[0].title;
+            // newActiveVideoObj.channel = newActiveVideo[0].channel;
+            // newActiveVideoObj.image = newActiveVideo[0].image;
+            // console.log('newActiveVideoObj: ', newActiveVideoObj);
+            // console.log('newActiveVideo: ', newActiveVideo);
+            let newVideos =
+                this.state.videos.filter(video => video.id !== rprops.match.params.id);
+            // console.log('newVideos (before push): ', newVideos);
+            // console.log('this.state.activeVideo (before push): ', this.state.activeVideo);
+            newVideos.push(this.state.activeVideo);
+            // console.log('newVideos (after push): ', newVideos);
             axios.get(window.$BF_URL + window.$BF_VIDEOS + video.id + window.$BF_API_KEY)
                 .then(result => {
-                    this.setState({ mainvideo: result.data })
+                    this.setState({ activeVideoDetail: result.data })
+                    this.setState({ activeVideo: newActiveVideo })
+                    this.setState({ videos: newVideos })
                 })
                 .catch(err => console.log('Error=>', err.response))
         }
@@ -66,14 +94,12 @@ class Home extends React.Component {
 
         return (
             <div>
-                {/* {console.log('Home.jsx render (MainVideo) -> this.state.mainvideo', this.state.mainvideo)} */}
-                <MainVideo videoObj={this.state.mainvideo} />
+                <MainVideo videoObj={this.state.activeVideoDetail} />
                 <main className="main">
                     <div className="main__left">
-                        <VideoDesc videoObj={this.state.mainvideo} />
+                        <VideoDesc videoObj={this.state.activeVideoDetail} />
                         <NewComment />
-                        {/* {console.log('Home.jsx render (CommentList) -> this.state.mainvideo.comments', this.state.mainvideo.comments)} */}
-                        <CommentList videoComments={this.state.mainvideo.comments} />
+                        <CommentList videoComments={this.state.activeVideoDetail.comments} />
                     </div>
                     <div className="main__right">
                         <SideVideoList videoList={this.state.videos} />
